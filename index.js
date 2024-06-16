8e1003a83430691c725f785d48638f5fd1733806
ctduong30666 <duongctfct30666@gmail.com>
2024-06-16 15:44:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const textElement = document.querySelector('.typing-text');
-    const text = 'Greeneration';
-    const coloredText = 'Green<span class="green-text">eration</span>';
+    const text = 'Greeeration';
+    const coloredText = 'Gree<span class="green-text">eration</span>';
     let index = 0;
     let isAdding = true;
     const typingSpeed = 100; // Tốc độ gõ nhanh hơn
@@ -11,9 +11,9 @@ document.addEventListener('DOMContentLoaded', function() {
         if (isAdding) {
             if (index < text.length) {
                 let currentText = text.substring(0, index + 1);
-                if (currentText.includes('Green')) {
-                    textElement.innerHTML = 'Green' + text.substring(4, index + 1);
-                    textElement.innerHTML = textElement.innerHTML.replace('Green', '<span class="green-text">Green</span>');
+                if (currentText.includes('Gree')) {
+                    textElement.innerHTML = 'Gree' + text.substring(4, index + 1);
+                    textElement.innerHTML = textElement.innerHTML.replace('Gree', '<span class="green-text">Gree</span>');
                 } else {
                     textElement.innerHTML = currentText;
                 }
@@ -26,9 +26,9 @@ document.addEventListener('DOMContentLoaded', function() {
         } else {
             if (index > 0) {
                 let currentText = text.substring(0, index - 1);
-                if (currentText.includes('Green')) {
-                    textElement.innerHTML = 'Green' + text.substring(4, index - 1);
-                    textElement.innerHTML = textElement.innerHTML.replace('Green', '<span class="green-text">Green</span>');
+                if (currentText.includes('Gree')) {
+                    textElement.innerHTML = 'Gree' + text.substring(4, index - 1);
+                    textElement.innerHTML = textElement.innerHTML.replace('Gree', '<span class="green-text">Gree</span>');
                 } else {
                     textElement.innerHTML = currentText;
                 }
